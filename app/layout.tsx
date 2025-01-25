2332381e9bafedc682dc59b22052d75733557a76
alexjava92 <autoexpert37@yandex.ru>
2025-01-25 12:45:55 +0300

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import theme from "../styles/theme";
 import Header from "@/componets/header/Header";
 import Footer from "@/componets/footer/Footer";
 import styles from "./Layout.module.css";
+import Head from "next/head";
+import Script from "next/script";
 
 const Wrapper = ({ children }: { children: ReactNode }) => {
     return (
@@ -20,6 +22,18 @@ const Wrapper = ({ children }: { children: ReactNode }) => {
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="ru">
+        <Head>
+            <meta name="yandex-verification" content="56c4a600fd4e1dbe" />
+            <noscript>
+                <div>
+                    <img
+                        src="https://mc.yandex.ru/watch/99657688"
+                        style={{ position: 'absolute', left: '-9999px' }}
+                        alt=""
+                    />
+                </div>
+            </noscript>
+        </Head>
         <body>
         <ThemeProvider theme={theme}>
             <GlobalStyles />
